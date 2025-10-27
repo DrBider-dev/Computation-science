fdda83ae28ec37d84ea32454ccbd44806cf928d0
DrBider <148402185+DrBider-dev@users.noreply.github.com>
2025-10-27 09:45:58 -0500

    agregando cositas de chatgpt para electron

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = 
+require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow( {
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+    
+        }
+    });
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
